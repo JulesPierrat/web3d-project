2a44ea9e270a7ce7bd2fdd014f4f8737daaf29a0
JulesPierrat <jules.pierrat@ensg.eu>
2022-02-04 15:55:25 +0100

    Microphone

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,15 @@
 var scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 var Mic = new Microphone();
 
 var render = function () {
-    requestAnimationFrame( render );
-    console.log(Mic.spectrum);
-
+    requestAnimationFrame(render);
+    
 };
 
 render();
