cb70caf9eb80298b4d62d7deb6152096d99a902a
JulesPierrat <jules.pierrat@ensg.eu>
2022-02-04 16:16:17 +0100

    LowPassFilter

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,27 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var Mic = new Microphone();
+var Mic = new Microphone(256);
+var LPF = new LowPassFilter(0.1);
+
+// Cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
 
 var render = function () {
     requestAnimationFrame(render);
+    let volume = LPF.filter(Mic.getRMS());
+    
+
+    cube.position.y += volume/50;
+    renderer.render( scene, camera );
+    cube.position.y -= volume/50;
+
+
     
 };
 
