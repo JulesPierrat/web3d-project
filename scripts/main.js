bcfbf44bcc9f7ce058407f51e1d963b3b1b984d5
JulesPierrat <jules.pierrat@ensg.eu>
2022-02-04 13:36:11 +0100

    Microphone

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
+var Mic = new Microphone();
 
 
 var render = function () {
