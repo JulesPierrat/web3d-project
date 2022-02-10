1300db2dbccc0fc12c76395cbe1ba00daf610ea7
Charles0009 <charles.laverdure@numericable.fr>
2022-02-10 09:12:49 +0100

    research work on the library for colors

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,13 @@ import * as THREE from '../lib/threejs/build/three.module.js';
 import { OrbitControls } from '../lib/threejs/examples/jsm/controls/OrbitControls.js';
 import { BuildingLoader} from "./BuildingLoader.js";
 import { OSMLoader} from "./OSMLoader.js";
+import { EffectComposer } from './lib/threejs/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './lib/threejs/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from './lib/threejs/examples/jsm/postprocessing/UnrealBloomPass.js';
+import { GUI } from '../lib/threejs/examples/jsm/libs/lil-gui.module.min.js';
+
+
+
 
 // Set scene
 var scene = new THREE.Scene();
@@ -13,6 +20,16 @@ var renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+
+let composer;
+
+const params = {
+    exposure: 1,
+    bloomStrength: 1.5,
+    bloomThreshold: 0,
+    bloomRadius: 0
+};
 
 
 // Microphone
@@ -46,6 +63,62 @@ loader.load("./data/manhattan-1854.geojson", 10000).then(a => {
     var osm = new OSMLoader();
     osm.load(loader.m_lon, loader.m_lat, 4);
 });
+
+
+// add color neons postprocess
+const renderScene = new RenderPass( scene, camera );
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
+				bloomPass.threshold = params.bloomThreshold;
+				bloomPass.strength = params.bloomStrength;
+				bloomPass.radius = params.bloomRadius;
+
+				composer = new EffectComposer( renderer );
+				composer.addPass( renderScene );
+				composer.addPass( bloomPass );
+
+
+const gui = new GUI();
+
+gui.add( params, 'exposure', 0.1, 2 ).onChange( function ( value ) {
+
+    renderer.toneMappingExposure = Math.pow( value, 4.0 );
+
+} );
+
+gui.add( params, 'bloomThreshold', 0.0, 1.0 ).onChange( function ( value ) {
+
+    bloomPass.threshold = Number( value );
+
+} );
+
+gui.add( params, 'bloomStrength', 0.0, 3.0 ).onChange( function ( value ) {
+
+    bloomPass.strength = Number( value );
+
+} );
+
+gui.add( params, 'bloomRadius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
+
+    bloomPass.radius = Number( value );
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 camera.position.x = 0;
 camera.position.y = 0;
